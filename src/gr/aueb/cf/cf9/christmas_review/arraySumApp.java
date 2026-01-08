69c36e8e416ae98d3b61cd2219856bbc7f6fbe00
pants <pants.ath@gmail.com>
2026-01-08 20:22:19 +0200

    feat: add junior and senior array sum App

---
+++ src/gr/aueb/cf/cf9/christmas_review/arraySumApp.java
@@ -0,0 +1,29 @@
+package gr.aueb.cf.cf9.christmas_review;
+
+/**
+ * Δίνεται ένας πίνακας ακεραίων.
+ * Να υπολογιστεί το άθροισμα των στοιχείων
+ * του.
+ */
+public class arraySumApp {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 20, 3};
+
+        int sum = sumArray(arr);
+
+        for (int e : arr) {
+            System.out.print(e + " ");
+        }
+        System.out.println();
+        System.out.println("The sum of the above numbers is: " + sum);
+    }
+
+    public static int sumArray(int[] arr) {
+        int sum = 0;
+        for (int e : arr) {
+            sum += e;
+        }
+        return sum;
+    }
+}
---
+++ src/gr/aueb/cf/cf9/christmas_review/arraySumAppSenior.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.cf9.christmas_review;
+
+import java.util.Arrays;
+
+public class arraySumAppSenior {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int sumArray(int[] arr) {
+        return Arrays.stream(arr).sum();
+    }
+}
