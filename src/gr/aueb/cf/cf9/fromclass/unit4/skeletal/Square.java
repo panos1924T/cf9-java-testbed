9a29a297ebcfce100e25b2eb4a393ce8e23424e6
pants <pants.ath@gmail.com>
2026-01-18 20:51:44 +0200

    feat: add square class that extends abstract class and overrides methods

---
+++ src/gr/aueb/cf/cf9/fromclass/unit4/skeletal/Square.java
@@ -0,0 +1,23 @@
+package gr.aueb.cf.cf9.fromclass.unit4.skeletal;
+
+public class Square extends AbstractRectangle {
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    @Override
+    public double getArea() {
+        return super.getArea();
+    }
+
+    @Override
+    public double getPerimeter() {
+        return super.getPerimeter();
+    }
+
+    @Override
+    public boolean isSquare() {
+        return super.isSquare();
+    }
+}
