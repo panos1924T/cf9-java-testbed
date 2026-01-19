e5d4dc84ac872fd0666b2a45344f5e876782f3b2
pants <pants.ath@gmail.com>
2026-01-19 20:15:01 +0200

    feat: add testing builder pattern

---
+++ src/gr/aueb/cf/cf9/fromclass/unit5/Main.java
@@ -0,0 +1,25 @@
+package gr.aueb.cf.cf9.fromclass.unit5;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Book newBook = new Book.Builder()
+                .title("title")
+                .author("author")
+                .isbn("AB123")
+                .year("2000")
+                .category("category")
+                .publisher("publisher")
+                .build();
+
+        Book newerBook = new Book.Builder()
+                .title("The better")
+                .year("1998")
+                .build();
+
+
+        System.out.println(newBook);
+        System.out.println(newerBook);
+    }
+}
