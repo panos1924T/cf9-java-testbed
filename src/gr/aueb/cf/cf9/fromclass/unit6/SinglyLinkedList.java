bb33af790a86ff412e2545861691cf7f97e85e39
PanTs <pants.ath@gmail.com>
2026-01-26 22:04:05 +0200

    feat: add start of singly linked list

---
+++ src/gr/aueb/cf/cf9/fromclass/unit6/SinglyLinkedList.java
@@ -0,0 +1,17 @@
+package gr.aueb.cf.cf9.fromclass.unit6;
+
+public class SinglyLinkedList<Τ>{
+
+    private Node head;
+
+
+    public class Node<T> {
+
+        private T data;
+        private Node next;
+
+
+        public Node(T data) {}
+
+    }
+}
