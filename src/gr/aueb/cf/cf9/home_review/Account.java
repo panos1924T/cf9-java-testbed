c1302c36ffb4507c55db9f9b73020f6f5d7e2fa9
PanTs <pants.ath@gmail.com>
2026-03-16 18:28:33 +0200

    feat: initialize account java bean with getters, setters and toString

---
+++ src/gr/aueb/cf/cf9/home_review/Account.java
@@ -0,0 +1,51 @@
+package gr.aueb.cf.cf9.home_review;
+
+
+import java.math.BigDecimal;
+
+/**
+ *  Account java bean
+ */
+public class Account {
+
+    private int id;
+    private String iban;
+    private String ssn;
+    private BigDecimal balance;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban() {
+        this.iban = iban;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public String accountToString() {
+        return "(Iban: " + iban + "Ssn: " + ssn + "Balance: " + balance + ")";
+    }
+}
