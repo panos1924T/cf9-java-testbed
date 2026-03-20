b65758938fb45aa06c9a6c56814bb073ea542e2f
pantsath <pants.ath@gmail.com>
2026-03-20 20:14:42 +0200

    feat: add java doc

--- src/gr/aueb/cf/cf9/home_review/StaticFactory.java
+++ src/gr/aueb/cf/cf9/home_review/StaticFactory.java
@@ -2,6 +2,10 @@ package gr.aueb.cf.cf9.home_review;
 
 import java.util.Random;
 
+/**
+ * this class provides two static factory methods
+ * so they can be used when called.
+ */
 public class StaticFactory {
 
     private int x = 0;
@@ -14,10 +18,19 @@ public class StaticFactory {
         this.y = y;
     }
 
+    /**
+     * initializes a static factory point at (0,0);
+     * @return      point at (0,0);
+     */
     public static StaticFactory getZeroPoint() {
         return new StaticFactory(0,0);
     }
 
+    /**
+     * This method initializes a random static factory point
+     * using a seed for randomness;
+     * @return      random point(x,y);
+     */
     public static StaticFactory getRandomPoint() {
         final int seed = 101;
         Random random = new Random();
--- src/gr/aueb/cf/cf9/home_review/StaticFactoryMain.java
+++ src/gr/aueb/cf/cf9/home_review/StaticFactoryMain.java
@@ -1,5 +1,8 @@
 package gr.aueb.cf.cf9.home_review;
 
+/**
+ * this class applies what used in StaticFactory.java
+ */
 public class StaticFactoryMain {
 
     public static void main(String[] args) {
