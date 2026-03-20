2b2940bf09a4c8b72cf68da29a53f4bf2e68f28d
pantsath <pants.ath@gmail.com>
2026-03-20 20:11:01 +0200

    feat: add static factory main apply

---
+++ src/gr/aueb/cf/cf9/home_review/StaticFactoryMain.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.cf9.home_review;
+
+public class StaticFactoryMain {
+
+    public static void main(String[] args) {
+
+        StaticFactory point1 = StaticFactory.getZeroPoint();
+        StaticFactory point2 = StaticFactory.getRandomPoint();
+
+
+    }
+}
