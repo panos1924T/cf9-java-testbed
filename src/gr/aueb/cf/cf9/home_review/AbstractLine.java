c2d6489653e3a1eb61350f5985cc88fa02b25057
PanTs <pants.ath@gmail.com>
2026-03-24 17:54:24 +0200

    feat: add add skeletal abstract class with method override draw line

---
+++ src/gr/aueb/cf/cf9/home_review/AbstractLine.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.cf9.home_review;
+
+import java.util.Scanner;
+
+
+/**
+ * defines the method for drawing the line
+ */
+public class AbstractLine implements ILine{
+
+    Scanner input = new Scanner(System.in);
+
+    @Override
+    public void drawLine() {
+        System.out.println("Please give a length for the line you want to draw");
+        int length = input.nextInt();
+
+        for (int i = 0; i < length; i++) {
+            System.out.print("-");
+        }
+    }
+}
