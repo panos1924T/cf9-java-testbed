1148033350c4bf20afcb795c8efed345fd35f16d
PanTs <pants.ath@gmail.com>
2026-03-24 18:27:44 +0200

    fix: refactor main for testing

--- src/gr/aueb/cf/cf9/home_review/interfaces/SkeletalMain.java
+++ src/gr/aueb/cf/cf9/home_review/interfaces/SkeletalMain.java
@@ -1,13 +1,20 @@
 package gr.aueb.cf.cf9.home_review.interfaces;
 
 
+import java.util.Scanner;
+
 /**
  * testing the skeletal interfaces
  */
 public class SkeletalMain {
 
     public static void main(String[] args) {
-        ILine line = new AbstractLine();
-        line.drawLine();
+
+        Scanner input = new Scanner(System.in);
+        ILine line = new DashLine();
+
+        System.out.println("Please enter the length for the line you want to draw: ");
+        int length = input.nextInt();
+        line.drawLine(length);
     }
 }
