3ea198e1fcbe1e08dcd98e0ed295e8ae643a7ce8
PanTs <pants.ath@gmail.com>
2026-03-26 18:17:28 +0200

    feat: initialize two classes that implement mixin interface printable

---
+++ src/gr/aueb/cf/cf9/home_review/lamda/excercise/Document.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.cf9.home_review.lamda.excercise;
+
+public class Document implements Printable {
+
+    @Override
+    public void print() {
+        System.out.println("Printing Document");
+    }
+
+}
---
+++ src/gr/aueb/cf/cf9/home_review/lamda/excercise/Photo.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.cf9.home_review.lamda.excercise;
+
+public class Photo implements Printable {
+
+    @Override
+    public void print() {
+        System.out.println("Printing photo");
+    }
+}
