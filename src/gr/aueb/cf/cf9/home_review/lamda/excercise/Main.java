615f2084c7c7443dc9a8e54b1d038b5497156e82
PanTs <pants.ath@gmail.com>
2026-03-26 18:29:33 +0200

    feat: initialize main and list of items

---
+++ src/gr/aueb/cf/cf9/home_review/lamda/excercise/Main.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.cf9.home_review.lamda.excercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Object> items = new ArrayList<>();
+        items.add(new Document());
+        items.add(new Photo());
+        items.add("Default");
+
+
+    }
+
+}
